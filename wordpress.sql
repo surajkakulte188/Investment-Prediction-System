056ac29c79d5ffe302e77a211ea1b2af3e1bd3f8
surajkakulte1808 <surajkakulte1808@gmail.com>
2025-06-08 14:16:44 +0530

    Create wordpress.sql

---
+++ wordpress.sql
@@ -0,0 +1,32 @@
+INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`) VALUES
+(484, 'wp_mail_smtp', 'a:6:{
+s:4:"mail";a:6:{
+s:10:"from_email";s:26:"suryak1803@gmail.com";
+s:9:"from_name";s:3:"JKS";
+s:6:"mailer";s:5:"gmail";
+s:11:"return_path";b:1;
+s:16:"from_email_force";b:0;
+s:15:"from_name_force";b:0;}
+s:4:"smtp";a:7:{
+s:7:"autotls";s:3:"yes";
+s:4:"auth";s:3:"yes";
+s:4:"host";s:0:"";
+s:10:"encryption";s:4:"none";
+s:4:"port";s:0:"";
+s:4:"user";s:0:"";
+s:4:"pass";s:0:"";}
+s:7:"mailgun";a:3:{s:7:"api_key";s:0:"";s:6:"domain";s:0:"";s:6:"region";s:2:"US";}
+s:8:"sendgrid";a:1:{s:7:"api_key";s:0:"";}
+s:10:"sendinblue";a:1:{s:7:"api_key";s:0:"";}
+s:5:"gmail";a:5:{
+s:9:"client_id";s:20:"[REDACTED_CLIENT_ID]";
+s:13:"client_secret";s:10:"[REDACTED]";
+s:9:"auth_code";s:10:"[REDACTED]";
+s:12:"access_token";a:6:{
+s:12:"access_token";s:10:"[REDACTED]";
+s:10:"expires_in";i:3599;
+s:5:"scope";s:24:"https://mail.google.com/";
+s:10:"token_type";s:6:"Bearer";
+s:7:"created";i:1614490367;
+s:13:"refresh_token";s:10:"[REDACTED]";}
+s:13:"refresh_token";s:10:"[REDACTED]";}}', 'no');
